2a93231fee703ec6ecfd201c9fcef551ffe23529
rklogan <ryan.logan@mail.mcgill.ca>
2020-12-28 15:20:20 -0500

    updated dependencies

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class CombinerAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    CombinerAudioProcessorEditor (CombinerAudioProcessor&);
+    ~CombinerAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    CombinerAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CombinerAudioProcessorEditor)
+};
